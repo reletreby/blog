f84426b9315b814ea90d5bd34ed39032716297f3
Rashad Eletreby <eletreby.rashad@gmail.com>
2025-01-03 12:27:54 -0500

    shorten bio

--- app/page.tsx
+++ app/page.tsx
@@ -10,16 +10,10 @@ export default function Page() {
               {`Hi, I'm Rashad Eletreby, a Principal Data Scientist and Senior Data Science Manager at Walmart, Inc. I grew up in Gizah, Egypt, and earned my B.Sc. and M.Sc. in Electrical and Computer Engineering from Cairo University. Later, I moved to the U.S. and completed my M.Sc. and Ph.D. in the same field at Carnegie Mellon University.`}
             </p>
             <p>
-              {`I'm passionate about learning and teaching, which inspired me to start this blog. It's a space where I share what I've learned—and continue to learn—about data science, technology, and more.`}
+              {`I'm passionate about learning and teaching, which inspired me to start this blog. It's a space where I share what I've learned—and continue to learn—about data science, technology, and more. Everything here is MIT licensed, so feel free to use and share it. If you have feedback, I'd love to hear from you!`}
             </p>
             <p>
-              {`Everything here is MIT licensed, so feel free to use and share it. If you have feedback, I'd love to hear from you!`}
-            </p>
-            <p>
-              {`The opinions expressed here are my own and do not represent the views of my current employer.`}
-            </p>
-            <p>
-              {`I write most of the content myself, but I also make use of tools like ChatGPT and Claude to clean up what I write or to contribute specific sections.`}
+              {`The opinions expressed here are my own and do not represent the views of my current employer. I write most of the content myself, but I also make use of tools like ChatGPT and Claude to clean up what I write or to contribute specific sections.`}
             </p>
             <p>
               {`If you'd like to connect, feel free to reach out at eletreby.rashad@gmail.com.`}
